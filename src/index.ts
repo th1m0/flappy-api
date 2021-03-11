c460ecadc099f335ed38c8dba53541d9efae629c
Thiimo <thimosietsma@gmail.com>
2021-03-11 15:58:53 +0100

    test github actions

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import app from "./app";
 
 const port = 5555;
 app.listen(port, () => {
-	console.log(`Listening: http://localhost:${port}`);
+	console.log(`Listening: http://localhost:${port}!`);
 });
