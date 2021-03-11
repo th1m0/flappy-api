d95c40a41cf72ee3badd8bee938a08d490009601
Thiimo <thimosietsma@gmail.com>
2021-03-11 16:01:00 +0100

    removed old code

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,18 +1,3 @@
-import express, { Request, Response } from "express";
-import server from "./server";
-import mcprison from "./mcprison";
+// import express, { Request, Response } from "express";
 
-const servers: String[] = ["arkham", "mcprison", "gotpvp"];
-
-const router = express.Router();
-
-router.get("/", (req: Request, res: Response) => {
-	res.json({
-		servers: [...servers],
-	});
-});
-
-router.use("/server", server);
-router.use("/mcprison", mcprison);
-
-export default router;
+// export default router;
--- src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-import uuid from "./uuid";
-
-export { uuid };
-
-// export default utils.size === 0 ? new Error("Utils got loaded to fast. Could not load utils.") : utils;
