923b8e4277f1357ecadd28c89ae11072c5bb334f
Thiimo <thimosietsma@gmail.com>
2021-03-16 11:25:56 +0100

    loading database in async

--- src/app.ts
+++ src/app.ts
@@ -11,9 +11,9 @@ import mongodb from "./database";
 import middlewares from "./middlewares";
 
 const app = express();
-(() => {
+(async () => {
 	console.log("[DATABASE]", "Loading database");
-	mongodb("mongodb://localhost/flappy");
+	await mongodb("mongodb://localhost/flappy");
 })();
 
 app.use(
