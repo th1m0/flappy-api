2c18570e22657d4ca8bc03812bd1af219c04eb44
Thiimo <thimosietsma@gmail.com>
2021-03-16 11:13:18 +0100

    Changed to production

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 
-const env: string = null;
+const env: string = "production";
 
 function notFound(req: Request, res: Response, next: NextFunction) {
 	res.status(404);
